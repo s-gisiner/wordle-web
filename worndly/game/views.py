fc8cfab7f15300e8d2a31a2ce97a5affd61b3f92
Sam Gisiner <samuelgisiner@gmail.com>
2026-04-09 17:25:38 -0400

    wrote home page HTML template and CSS style sheet

--- worndly/game/views.py
+++ worndly/game/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
 def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = User.objects.filter(username=username, password=password).first()
+        if user:
+            return render(request, 'game/home.html')
     return render(request, 'game/login.html')
+
+def home(request):
+    return render(request, 'game/home.html')
