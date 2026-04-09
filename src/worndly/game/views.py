d828b342335b1b3029bcd4f2cb9445ec14359f99
Sam Gisiner <samuelgisiner@gmail.com>
2026-04-09 17:44:07 -0400

    added a view to create a new user

--- src/worndly/game/views.py
+++ src/worndly/game/views.py
@@ -9,7 +9,19 @@ def login(request):
         user = User.objects.filter(username=username, password=password).first()
         if user:
             return render(request, 'game/home.html')
+        
     return render(request, 'game/login.html')
+
+def register(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        User.objects.create(name=name, username=username, email=email, password=password)
+        return render(request, 'game/login.html')
+    
+    return render(request, 'game/signup.html')
 
 def home(request):
     return render(request, 'game/home.html')
