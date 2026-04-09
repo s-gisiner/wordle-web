decf3074e5a6859eb06295e1b2bb4fdfe62d969f
Sam Gisiner <samuelgisiner@gmail.com>
2026-04-09 18:06:37 -0400

    improved logout

--- src/worndly/game/urls.py
+++ src/worndly/game/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 app_name = 'game'  # creates a namespace for this app
@@ -6,4 +7,5 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('home/', views.home, name='home'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='game:login'), name='logout'),
 ]
