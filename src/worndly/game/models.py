a052deced243c0cf231020a5b834fffae8513269
Sam Gisiner <samuelgisiner@gmail.com>
2026-04-10 13:33:25 -0400

    wrote CONTRIBUTIONS.md and made name field optional in User

--- src/worndly/game/models.py
+++ src/worndly/game/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class User(AbstractUser):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, blank=True)
     email = models.EmailField(unique=True)
 
     def __str__(self):
